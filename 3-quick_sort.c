3092f52f7ad31cbfc2ba1c94e483305a4191bfe1
IVanEDL <ivanaq3d@gmail.com>
2023-01-10 16:01:57 -0800

    lets see idk

---
+++ 3-quick_sort.c
@@ -0,0 +1,85 @@
+#include "sort.h"
+
+/*
+ * quick_sort - orders an array, quickly
+ * @array: received array
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array && size > 0)
+		quicksort_recursion(&array, 0, size - 1, size);
+}
+
+/*
+ * swap - swaps elements of an array
+ * @i: first element
+ * @a: second element
+ * @size: size of the array
+ */
+
+void swaper(int *array, size_t i, size_t a, size_t size)
+{
+	size_t temp;
+
+	temp = array[a];
+	array[a] = array[i];
+	array[i] = temp;
+	print_array(array, size);
+}
+
+/*
+ * quicksort_recursion - truly orders an array
+ * @array: array
+ * @low: low
+ * @high: high
+ */
+
+void quicksort_recursion(int **array, size_t low, size_t high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(*array, low, high, size);
+		if (pivot = 0)
+			pivot = high + 1;
+		if (pivot = -1)
+			return;
+		quicksort_recursion(array, low, part - 1, size);
+		quicksort_recursion(array, part + 1, high, size);
+	}
+}
+
+/*
+ * partition - does the windy thing
+ * @array: array to partitionate
+ * @low: atl
+ * @high: PB
+ * @size: size of the array
+ * Return: index of the pivot
+ */
+
+size_t partition(int *array, size_t low, size_t high, size_t size)
+{
+	size_t i, a = low - 1, eq = 0;
+	int pivot = array[high];
+
+	for (i = low; i < high; i++)
+	{
+		if (array[i] < pivot)
+		{
+			a++;
+			if (array[a] != array[i])
+				swap(array, i, a, size);
+		}
+		if (array[i] == array[i + 1])
+			eq++;
+	}
+	if (eq = size - 1)
+		return - 1;
+	if (array[high] != array[a + 1])
+		swap(array, high, a + 1, size);
+	return (a + 1);
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,9 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+void swaper(int *array, size_t i, size_t a, size_t size);
+void quicksort_recursion(int **array, size_t low, size_t high);
+size_t partition(int *array, size_t low, size_t high, size_t size)
 
 #endif
